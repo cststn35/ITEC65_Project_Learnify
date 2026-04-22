91776fd371b7f9e28ba0032520880cf9d39cec3b
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-04-23 00:13:50 +0800

    added addt'l styling for the login/registration

--- login-register.php
+++ login-register.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./assets/css/login-register.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
