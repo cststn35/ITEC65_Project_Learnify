34f4ec507c8911ae9a80df4a2922cc547e0e0fe7
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-05-02 01:08:31 +0800

    Added the initial structure for the dashboard

---
+++ pages/dashboard.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once("meta-head.php") ?>
+
+<body class="font-[Inter]">
+    <div class="md:grid md:grid-cols-[250px_1fr] md:grid-rows-[60px_1fr] h-screen">
+        <?php
+        $pageTitle = "Dashboard";
+        include_once("topsidebar.php")
+            ?>
+        <main class="bg-gray-200 col-start-2 p-20 h-full">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+                software like Aldus PageMaker including versions of Lorem Ipsum</p>
+        </main>
+    </div>
+</body>
+
+</html>
