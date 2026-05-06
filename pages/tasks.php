b24cea2750d121ab399ed8d915fdb67ba55bbf9b
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-05-06 21:59:40 +0800

    created the add task modal

--- pages/tasks.php
+++ pages/tasks.php
@@ -13,10 +13,89 @@
             <div class="flex justify-between">
                 <h1 class="font-bold text-2xl">TASKS</h1>
                 <button type="button"
-                    class="px-3.5 py-2 text-white text-sm font-semibold cursor-pointer bg-[#333] hover:bg-[#222] border border-[#333] rounded-md transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-[#444] flex items-center gap-2">
+                    class="px-3.5 py-2 text-white text-sm font-semibold cursor-pointer bg-[#333] hover:bg-[#222] border border-[#333] rounded-md transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-[#444] flex items-center gap-2" id="openModal">
                     <i class='bx bx-plus text-sm md:text-base'></i>
                     <span class="text-sm md:text-base">Add Task</span>
                 </button>
+            </div>
+
+            <!-- add task overlay -->
+            <div id="modalOverlay"
+                class="fixed inset-0 p-4 flex flex-wrap justify-center items-center w-full h-full z-1000 before:fixed before:inset-0 before:w-full before:h-full before:bg-[rgba(0,0,0,0.5)]">
+
+                <div role="dialog" aria-modal="true" aria-labelledby="modal-title" tabindex="-1"
+                    class="w-full max-w-xl bg-white border border-slate-100 shadow-lg rounded-lg relative max-h-[95vh] overflow-y-auto outline-none p-4 md:p-6">
+                    <div class="flex items-center pb-3 border-b border-slate-300">
+                        <h3 id="modal-title"
+                            class="text-slate-900 text-lg font-semibold flex-1 flex items-center gap-2"><i
+                                class='bx bxs-plus-square text-2xl'></i>Create Task
+                        </h3>
+
+                        <button type="button" id="closeModal" aria-label="Close modal"
+                            class="ml-auto flex items-center focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 rounded">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                class="size-3 cursor-pointer fill-slate-500 hover:fill-red-600" aria-hidden="true"
+                                viewBox="0 0 329.269 329">
+                                <path
+                                    d="M194.8 164.77 323.013 36.555c8.343-8.34 8.343-21.825 0-30.164-8.34-8.34-21.825-8.34-30.164 0L164.633 134.605 36.422 6.391c-8.344-8.34-21.824-8.34-30.164 0-8.344 8.34-8.344 21.824 0 30.164l128.21 128.215L6.259 292.984c-8.344 8.34-8.344 21.825 0 30.164a21.27 21.27 0 0 0 15.082 6.25c5.46 0 10.922-2.09 15.082-6.25l128.21-128.214 128.216 128.214a21.27 21.27 0 0 0 15.082 6.25c5.46 0 10.922-2.09 15.082-6.25 8.343-8.34 8.343-21.824 0-30.164zm0 0" />
+                            </svg>
+                        </button>
+                    </div>
+
+                    <form action="">
+                        <div class="my-6 space-y-6">
+                            <div class="titleInput">
+                                <label for="title" class="mb-2 text-slate-900 font-medium text-base inline-block">Title
+                                    <span class="text-red-500 font-bold">*</span></label>
+                                <input type="text" id="title" placeholder="e.g. Study Math Chapter 3"
+                                    class="px-3.5 py-3 text-base text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600" />
+                            </div>
+                            <div class="descriptionInput">
+                                <label for="description"
+                                    class="mb-2 text-slate-900 font-medium text-base inline-block">Short
+                                    Description
+                                    <span class="text-gray-500 font-bold text-xs">(optional)</span></label>
+                                <input type="text" id="description" placeholder="Add notes about this task"
+                                    class="px-3.5 py-3 text-base text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600" />
+                            </div>
+                            <div class="subjectInput">
+                                <label for="subject"
+                                    class="mb-2 text-slate-900 font-medium text-base inline-block">Subject
+                                    <span class="text-red-500 font-bold">*</span></label>
+                                <select name="subject" id="subject"
+                                    class="px-3.5 py-3 text-base text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600">
+                                    <option value="" disabled hidden selected>Select a subject</option>
+                                    <option value="">Math</option>
+                                    <option value="">Science</option>
+                                </select>
+                            </div>
+                            <div class="deadlineInput">
+                                <label for="deadline"
+                                    class="mb-2 text-slate-900 font-medium text-base inline-block">Deadline
+                                    <span class="text-red-500 font-bold">*</span></label>
+                                <input type="date" id="deadline" placeholder="Add notes about this task"
+                                    class="px-3.5 py-3 text-base text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600" />
+                            </div>
+                            <div class="timeInput">
+                                <label for="time"
+                                    class="mb-2 text-slate-900 font-medium text-base inline-block">Estimated Time To
+                                    Accomplish (in minutes)
+                                    <span class="text-gray-500 font-bold text-xs">(optional)</span></label>
+                                <input type="number" id="time" placeholder="e.g. 10"
+                                    class="px-3.5 py-3 text-base text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600" />
+                            </div>
+                        </div>
+
+                        <div class="border-t border-slate-300 pt-4 flex justify-end gap-4 md:pt-6">
+                            <button type="button" id="cancelBtn"
+                                class="px-3.5 py-2 text-slate-900 text-sm font-semibold rounded-md cursor-pointer bg-white border border-slate-300 transition-colors hover:bg-slate-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500">
+                                Cancel</button>
+                            <button type="button"
+                                class="px-3.5 py-2 text-white text-sm font-semibold rounded-md cursor-pointer bg-blue-600 border border-blue-600 transition-colors hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500">
+                                Create Task</button>
+                        </div>
+                    </form>
+                </div>
             </div>
 
             <!-- cards -->
@@ -243,6 +322,8 @@
             </div>
         </main>
     </div>
+
+    <script src="../assets/js/tasks.js"></script>
 </body>
 
 </html>
