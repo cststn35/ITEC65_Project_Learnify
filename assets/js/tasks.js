da2aed5727aad95ea4d81ad603a8b97e8db9f1fe
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-05-07 23:40:27 +0800

    fixed kebab behavior

--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -54,3 +54,31 @@ document.addEventListener("keydown", (e) => {
         }
     }
 });
+
+//dropdown menu logic
+
+document.addEventListener('click', function (e) {
+
+    const btn = e.target.closest('[data-action="dropdown-toggle"]'); //gets the button that was clicked. e.target could be the icon or the button itself. we should grab hold the button
+
+    // If clicking the button
+    if (btn) {
+        const card = btn.closest('.one-card'); //get the parent html of the button, a.k.a the card; returns an html
+        const dropdown = card.querySelector('[data-dropdown]'); //from the card itself only, find the dropdown; returns an html
+
+        // close others first
+        document.querySelectorAll('[data-dropdown]').forEach(d => {
+            if (d !== dropdown) d.classList.add('hidden');
+            //in each dropdown, we close that doesnt match the current dropdown to be opened. dropd
+        });
+
+        dropdown.classList.toggle('hidden');
+        return;
+    }
+
+    // If clicking outside
+    document.querySelectorAll('[data-dropdown]').forEach(d => {
+        d.classList.add('hidden');
+    });
+
+});
