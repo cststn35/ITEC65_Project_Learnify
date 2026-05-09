a1c9d08cbd1aa309bf4e3ca69b743538b182f446
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-05-09 22:32:54 +0800

    created a php file responsible for deleting courses

---
+++ actions/courses/delete_course.php
@@ -0,0 +1,51 @@
+<?php
+header("Content-Type: application/json");
+require_once __DIR__ . '/../../config/runQuery.php';
+
+try {
+    if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        $userID = isset($_GET["userID"])
+            ? trim($_GET["userID"])
+            : "";
+
+        $courseID = isset($_GET["course_id"])
+            ? trim($_GET["course_id"])
+            : "";
+
+        $semesterID = isset($_GET["semester_id"])
+            ? trim($_GET["semester_id"])
+            : "";
+
+        $sql = "UPDATE subjects
+        SET is_archived = 1
+        WHERE user_id = :userID 
+            AND semester_id = :semesterID
+            AND subject_id = :subjectID
+            AND is_archived = 0";
+
+        $params = [
+            "userID" => $userID,
+            "semesterID" => $semesterID,
+            "subjectID" => $courseID
+        ];
+
+        $result = runQuery($pdo, $sql, $params);
+
+        if ($result->rowCount() > 0) { //to confirm if it is deleted
+            echo json_encode([
+                "success" => true,
+                "message" => "Task deleted"
+            ]);
+        } else {
+            echo json_encode([
+                "success" => false,
+                "message" => "No changes made (same data or task not found)"
+            ]);
+        }
+    }
+} catch (PDOException $e) {
+    echo json_encode([
+        'success' => false,
+        'error' => $e->getMessage()
+    ]);
+}
