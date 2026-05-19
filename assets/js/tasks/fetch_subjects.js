c7afcd124e02b6f4660b66db391e6fb5a3bacbc1
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-05-20 01:26:26 +0800

    modified these files to give way to filtering functionality in task page

--- assets/js/tasks/fetch_subjects.js
+++ assets/js/tasks/fetch_subjects.js
@@ -1,32 +1,36 @@
 async function fetchSubjects(userID) {
-    console.log(userID);
-    const subjectInput = document.getElementById("subject");
-    try {
-        const response = await fetch(`/${BASE_URL}/actions/tasks/fetch_subjects.php?userID=${userID}&semester_id=${semesterID}`);
+  console.log(userID);
+  const subjectInput = document.getElementById("subject");
+  const subjectFilter = document.getElementById("subject-filter");
+  try {
+    const response = await fetch(
+      `/${BASE_URL}/actions/tasks/fetch_subjects.php?userID=${userID}&semester_id=${semesterID}`,
+    );
 
-        // check HTTP status first
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        if (data.success) {
-            console.log(data.data);
-
-            data.data.forEach(subject=>{
-                subjectInput.innerHTML += 
-                `<option value="${subject.subject_id}">${subject.name}</option>`;
-            });
-
-
-        } else {
-            console.log("Server returned error:", data.error || "Unknown error");
-        }
-
-    } catch (error) {
-        console.error("Fetch failed:", error);
+    // check HTTP status first
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    const data = await response.json();
+
+    if (data.success) {
+      console.log(data.data);
+      subjectInput.innerHTML =
+        '<option value="" disabled hidden selected>Select a subject</option>';
+      subjectFilter.innerHTML =
+        '<option value="all-subjects" selected>All Subject</option>';
+
+      data.data.forEach((subject) => {
+        subjectInput.innerHTML += `<option value="${subject.subject_id}">${subject.name}</option>`;
+        subjectFilter.innerHTML += `<option value="${subject.subject_id}">${subject.name}</option>`;
+      });
+    } else {
+      console.log("Server returned error:", data.error || "Unknown error");
+    }
+  } catch (error) {
+    console.error("Fetch failed:", error);
+  }
 }
 
 fetchSubjects(userID);
