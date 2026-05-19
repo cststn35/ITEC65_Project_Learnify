71c367a5d1517c459a280add4dc3c3324f5e273a
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-05-20 01:25:12 +0800

    made a js file responsible for marking tasks as done

---
+++ assets/js/tasks/complete_task.js
@@ -0,0 +1,28 @@
+async function completeTask(taskID, userID) {
+  const result = await Swal.fire({
+    title: "Complete Task?",
+    text: "Do you want to mark this task as done?",
+    icon: "question",
+
+    showCancelButton: true,
+
+    confirmButtonText: "Yes",
+    cancelButtonText: "Cancel",
+  });
+
+  if (!result.isConfirmed) return;
+
+  const response = await fetch(
+    `/${BASE_URL}/actions/tasks/complete_task.php?userID=${userID}&semesterID=${semesterID}&taskID=${taskID}`,
+  );
+  const data = await response.json();
+  console.log(data);
+  if (data.success) {
+    Swal.fire({
+      icon: "success",
+      title: "Marked As Done!",
+      text: "The task has been successfully marked as done",
+    });
+    fetch_task("from swal of complete task");
+  }
+}
