ea90b5ca6b2786972963af4542a69c26f37a1ea4
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-05-30 22:48:36 +0800

    changed some structure in the notifications table

--- config/initdb.php
+++ config/initdb.php
@@ -271,16 +271,13 @@ CREATE TABLE IF NOT EXISTS notifications (
     user_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     message TEXT NOT NULL,
-    type ENUM(
-        'REMINDER',
-        'DEADLINE',
-        'STREAK',
-        'SYSTEM'
-    ) NOT NULL,
+    type VARCHAR(100) NOT NULL,
     is_read TINYINT(1) DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    semester_id INT NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (semester_id) REFERENCES semesters(semester_id)
 );
 "
 );
