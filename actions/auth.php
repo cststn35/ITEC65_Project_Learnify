3d37c56d82accf4a98433ed97909a676f62de8ad
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-06-05 19:46:50 +0800

    added newly registered authentication

--- actions/auth.php
+++ actions/auth.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+if (($_SESSION['is_newly_registered']) == 1) {
+    header("Location: ../pages/welcome_wizard.php");
+    exit;
+}
+
 if (!isset($_SESSION['user_id'])) {
     // relative path back to login.php in parent folder
     header("Location: ../login-register.php");
