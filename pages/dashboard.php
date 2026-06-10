6d3c4da3342fbd370f54dbd595b3a3eb3003bbb7
Irron Madlangbayan <roncodmm@gmail.com>
2026-06-10 10:09:43 +0800

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -132,7 +132,7 @@ date_default_timezone_set('Asia/Manila');
                                     <option value="3">Tuesday</option>
                                     <option value="4">Wednesday</option>
                                     <option value="5">Thursday</option>
-                                    <option value="6">Fry</option>
+                                    <option value="6">Friday</option>
                                     <option value="7">Sa</option>
                                 </select>
                             </div>
