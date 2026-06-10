dad9dc4a086c32596dcecac9d77b92387fb719ba
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-06-10 22:53:08 +0800

    made a php file that processes name change

---
+++ components/change-name.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require '../config/runQuery.php';
+
+header('Content-Type: application/json');
+
+$_first_name = trim($_POST['first_name'] ?? '');
+$_last_name = trim($_POST['last_name'] ?? '');
+
+
+$user_id = $_SESSION['user_id'];
+
+if (empty($_first_name) || empty($_last_name)) {
+    echo json_encode([
+        'success' => false,
+        'message' => 'First name and last name cannot be empty.'
+    ]);
+    exit;
+}
+
+$sql = "UPDATE users SET first_name = :first_name, last_name = :last_name WHERE user_id = :user_id";
+runQuery($pdo, $sql, [
+    'first_name' => $_first_name,
+    'last_name' => $_last_name,
+    'user_id' => $user_id
+]);
+
+
+echo json_encode([
+    'success' => true,
+    'message' => 'Name updated successfully.'
+]);
