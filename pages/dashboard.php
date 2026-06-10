14f36b1bffad7a4122c9dc3322986b3556f46e65
Irron Madlangbayan <roncodmm@gmail.com>
2026-06-10 09:35:08 +0800

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -127,7 +127,7 @@ date_default_timezone_set('Asia/Manila');
                                 <select name="day" id="day" required
                                     class="px-3.5 py-3 text-base text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600">
                                     <option value="" disabled hidden selected>Select a day</option>
-                                    <option value="1"></option>
+                                    <option value="1">Sunday</option>
                                     <option value="2">Monday</option>
                                     <option value="3">Tuesday</option>
                                     <option value="4">Wednesday</option>
