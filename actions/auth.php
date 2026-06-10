c8be925294d252afb074b6e70fda931a76b3378e
Carlos Satsatin <carlossatsatin15@gmail.com>
2026-06-10 22:43:56 +0800

    added newly reg flag for auth check

--- actions/auth.php
+++ actions/auth.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
 
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['is_newly_registered'])) {
+    // relative path back to login.php in parent folder
+    header("Location: ../login-register.php");
+    exit;
+}
+
 if (($_SESSION['is_newly_registered']) == 1) {
     header("Location: ../pages/welcome_wizard.php");
     exit;
